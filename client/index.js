435f8a055b3d39bbcba3bf716b2a839114f78c03
Abdul Ghaffar <aghaffar570@gmail.com>
2018-09-01 21:15:26 -0400

    Basic [react, redux, express] setup

---
+++ client/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router } from 'react-router-dom';
+import history from './history';
+import store from './store';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <div>hello react</div>
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+);
